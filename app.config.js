31c08eb1423368b4cf01df18167797cc3a09d267
rpowis <rorypowis@gmail.com>
2025-01-07 11:18:01 +0100

    Try SPA to fix build async storage build error

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ export default {
     },
     web: {
       bundler: "metro",
-      output: "static",
+      output: "single",
       favicon: "./assets/images/favicon.png",
     },
     plugins: [
